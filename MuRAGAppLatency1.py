ef2e772f29ba5ea8234311e9406bd57137c92af5
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-20 16:28:22 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -637,7 +637,7 @@ if uploaded_file is not None:
                       found_image = True  # Set the flag to True to indicate that an image has been found
                       
                       break
-                  client.delete_collection("mm_rag_mistral04") 
+                  #client.delete_collection("mm_rag_mistral04") 
         
           
         
